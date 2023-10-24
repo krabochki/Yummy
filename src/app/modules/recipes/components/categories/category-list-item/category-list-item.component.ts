436fd08c152bf9d89ba2af2c1319248950fa32cc
Полина <pollulsd@mail.ru>
2023-10-25 00:34:21 +0300

    кол-во рецептов и ссылки в т ч для мини-секций, количество рецептов  в категориях теперь считается не только по всем рецептам, но и по приватным и ожидающим своим

--- src/app/modules/recipes/components/categories/category-list-item/category-list-item.component.ts
+++ src/app/modules/recipes/components/categories/category-list-item/category-list-item.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 import { RecipeService } from '../../../services/recipe.service';
+import { AuthService } from 'src/app/modules/authentication/services/auth.service';
 
 @Component({
   selector: 'app-category-list-item',
@@ -16,24 +17,32 @@ export class CategoryListItemComponent implements OnInit {
   @Input() context: 'section' | 'category' = 'category';
 
   recipesNumber = 0;
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private authService: AuthService,
+  ) {}
   ngOnInit() {
     if (this.showRecipesNumber)
       this.recipeService.recipes$.subscribe((data) => {
-        if (this.context === 'category')
-        
-          this.recipesNumber = this.recipeService.getRecipesByCategory(
-            this.recipeService.getPublicRecipes(data),
-            this.category.id,
-          ).length;
-        else {
-          this.category.categoriesId.forEach((element: number) => {
-            this.recipesNumber += this.recipeService.getRecipesByCategory(
-              this.recipeService.getPublicRecipes(data),
-              element,
+        this.authService.currentUser$.subscribe((user) => {
+          if (this.context === 'category')
+            this.recipesNumber = this.recipeService.getRecipesByCategory(
+              this.recipeService.getPublicAndAllMyRecipes(data,user.id),
+              this.category.id,
             ).length;
-          });
-        }
-      });
+          else {
+            this.category.categoriesId.forEach((element: number) => {
+              this.recipesNumber += this.recipeService.getRecipesByCategory(
+                this.recipeService.getPublicAndAllMyRecipes(data, user.id),
+                element,
+              ).length;
+            });
+          }
+        });
+
+
+        });
+
+        
   }
 }
